ad728f0e01caae2f5f90450a518226e1bc2474ae
ylocinn <115472908+ylocinn@users.noreply.github.com>
2022-10-11 10:10:42 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ var result;
 var count = 0;
 function digitar() {
   result = document.getElementById("result");
-  window.setTimeout(function() { inserir(texto[count]) }, 5000);
+  window.setTimeout(function() { inserir(texto[count]) }, 5);
 }
 
 function inserir(letra) {
   result.innerHTML += letra;
   count++;
   if(count < texto.length)
-    window.setTimeout(function() { inserir(texto[count]) }, 5000);
+    window.setTimeout(function() { inserir(texto[count]) }, 5);
 }
 
 window.onload = digitar;
