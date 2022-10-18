2588d0b44c9a4780b7766c23210ae08358f49634
maou4869 <115473213+maou4869@users.noreply.github.com>
2022-10-18 10:09:07 -0300

    Create index.js

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ var result;
 var count = 0;
 function digitar() {
 result = document.getElementById("result");
-window.setTimeout(function() { inserir(texto[count]) }, 50);
+window.setTimeout(function() { inserir(texto[count]) }, 30);
 }
 
 function inserir(letra) {
 result.innerHTML += letra;
 count++;
 if(count < texto.length)
-window.setTimeout(function() { inserir(texto[count]) }, 50);
+window.setTimeout(function() { inserir(texto[count]) }, 30);
 }
 
 window.onload = digitar;
