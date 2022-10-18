8611ee9907aa75a811e30c374b39e0192b001e60
matt-integrador <115473561+matt-integrador@users.noreply.github.com>
2022-10-18 10:03:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var texto = "A Revolução Francesa aconteceu entre os anos de 1789 e 1799, foi um período progressista inspirada em ideais do Iluminismo e incentivada por um cenário de crise, vivido pela França no final do século XVIII. Ocasionou grandes transformações e designou o começo da queda do absolutismo europeu.";
+var texto = "A Revolução Francesa aconteceu entre os anos de 1789 e 1799, foi um período progressista inspirada em ideais do Iluminismo e incentivada por um cenário de crise, vivido pela França no final do século XVIII. Ocasionou grandes transformações e designou o começo da queda do absolutismo europeu. As ruas francesas podem ser encantadoras, porém isso depende de quem você é. . .";
 var result;
 
 var count = 0;
