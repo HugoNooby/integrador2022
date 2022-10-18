26479b587100689824b5ec1360042f2d6a79d5b6
maou4869 <115473213+maou4869@users.noreply.github.com>
2022-10-18 10:48:12 -0300

    Create jogador.js

---
+++ jogador.js
@@ -0,0 +1,15 @@
+var id = null;
+function myMove() {
+  var elem = document.getElementById("nome_usuario");   
+  var pos = 0;
+  clearInterval(id);
+  id = setInterval(frame, 10);
+  function frame() {
+    if (pos == 10) {
+      clearInterval(id);
+    } else {
+      pos++; 
+      elem.style.opacity = pos*0.1; 
+    }
+  }
+}
