55b29afeae782d268f360eaf0b941cffc9e93914
maou4869 <115473213+maou4869@users.noreply.github.com>
2022-10-24 09:54:24 -0300

    Update Lara.js

--- Lara.js
+++ Lara.js
@@ -1,5 +1,5 @@
 let toggle = document.querySelector('.toggle');
 let menu = document.querySelector('.menu');
 toggle.onclick = function(){
-    menu.classlist.toggle('active')
+    menu.classlist.add('active')
 }
