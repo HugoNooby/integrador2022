1cb0f9b782abcc12183011139439b976e742c00c
matt-integrador <115473561+matt-integrador@users.noreply.github.com>
2022-11-02 17:27:22 -0300

    Update Bispo_Sieyès.js

--- Bispo_Sieyès.js
+++ Bispo_Sieyès.js
@@ -1,4 +1,4 @@
-var texto = "Parabéns "nome do usuário" você agora é o Bispo Emmanuel Joseph Sieyès, sua história começará em 1 2 3 . . . É julho de 1789, todos estão reunidos no palácio, o rei sentado ao trono, o clero em suas rezas, a nobreza já em luto, e em frente, o terceiro estado. Uma sensação de reviravoltas invade o local. A proclamação da Assembleia Nacional Constituinte está acontecendo."
+var texto = "Parabéns 'nome do usuário' você agora é o Bispo Emmanuel Joseph Sieyès, sua história começará em 1 2 3 . . . É julho de 1789, todos estão reunidos no palácio, o rei sentado ao trono, o clero em suas rezas, a nobreza já em luto, e em frente, o terceiro estado. Uma sensação de reviravoltas invade o local. A proclamação da Assembleia Nacional Constituinte está acontecendo, Acredito que o Bispo Sieyès, para os íntimos Emmanuel Sieyès, seja opoente aos nobres. Ano passado publicou uma obra fascinante, depondo que a nobreza seria um fardo para nossa economia, graças aos seus gastos mirabolantes, 'Vou para fora tomar um pouco de ar', pensou o bispo levantando-se e saindo pela porta ao fundo. Observe que, enquanto um membro está a proclamar da assembleia, é divertido analisar as diferentes expressões que cada grupo realiza, alguns parecem decepcionados, outros satisfeitos e ainda outros surpresos, mas não tanto quanto Emmanuel Sieyès ao voltar de seu passeio, parece que descobriu algo inesperado... ou nem tanto assim.."
 var primeira_escolha;
 
 var count = 0;
@@ -17,20 +17,4 @@ window.setTimeout(function() { inserir(texto[count]) }, 300);
 window.onload = digitar;
 
 
-var texto2 = "Acredito que o Bispo Sieyès, para os íntimos Emmanuel Sieyès, seja opoente aos nobres. Ano passado publicou uma obra fascinante, depondo que a nobreza seria um fardo para nossa economia, graças aos seus gastos mirabolantes, 'Vou para fora tomar um pouco de ar', pensou o bispo levantando-se e saindo pela porta ao fundo. Observe que, enquanto um membro está a proclamar da assembleia, é divertido analisar as diferentes expressões que cada grupo realiza, alguns parecem decepcionados, outros satisfeitos e ainda outros surpresos, mas não tanto quanto Emmanuel Sieyès ao voltar de seu passeio, parece que descobriu algo inesperado... ou nem tanto assim."
-var primeira_escolha2;
 
-var count = 0;
-function digitar() {
-primeira_escolha = document.getElementById("primeira_escolha2");
-window.setTimeout(function() { inserir(texto[count]) }, 300);
-}
-
-function inserir(letra) {
-primeira_escolha.innerHTML += letra;
-count++;
-if(count < texto.length)
-window.setTimeout(function() { inserir(texto[count]) }, 300);
-}
-
-window.onload = digitar;
