c633f4832347076b4792cb09901f522d4b71d7d1
matt-integrador <115473561+matt-integrador@users.noreply.github.com>
2022-11-02 17:38:34 -0300

    Update Bispo_Sieyès.js

--- Bispo_Sieyès.js
+++ Bispo_Sieyès.js
@@ -4,14 +4,14 @@ var primeira_escolha;
 var count = 0;
 function digitar() {
 primeira_escolha = document.getElementById("primeira_escolha");
-window.setTimeout(function() { inserir(texto[count]) }, 300000);
+window.setTimeout(function() { inserir(texto[count]) }, 3);
 }
 
 function inserir(letra) {
 primeira_escolha.innerHTML += letra;
 count++;
 if(count < texto.length)
-window.setTimeout(function() { inserir(texto[count]) }, 300000);
+window.setTimeout(function() { inserir(texto[count]) }, 3;
 }
 
 window.onload = digitar;
