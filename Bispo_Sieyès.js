ecb3d90af812ea2979092fae8a929eac7c453182
matt-integrador <115473561+matt-integrador@users.noreply.github.com>
2022-11-03 14:46:17 -0300

    Update Bispo_Sieyès.js

--- Bispo_Sieyès.js
+++ Bispo_Sieyès.js
@@ -1,13 +1,13 @@
 var texto = "É julho de 1789, todos estão reunidos no palácio, o rei sentado ao trono, o clero em suas rezas, a nobreza já em luto, e em frente, o terceiro estado. Uma sensação de reviravoltas invade o local. A proclamação da Assembleia Nacional Constituinte está acontecendo."
 var primeira_escolha;
 
-var textoviu = "O povo estava unido, seus instrumentos de trabalho estavam empunhados em suas cansadas mãos, um protesto, o início de uma revolução, foi o que viu através de uma janela no corredor real. Sinceramente, já esperávamos por isso. A desigualdade entre os três estados é nítida, uma grande crise econômica autora de fomes e mortes, pensamentos iluministas, influências americanas, a lista continua, tudo contribui para o momento em que nos encontramos agora. “Não há como impedir que, essas pessoas não façam o que já está consumindo seus corpos” novamente pensou o bispo. Mas você pode escolher o que elas farão. ."
+var textoviu = 'O povo estava unido, seus instrumentos de trabalho estavam empunhados em suas cansadas mãos, um protesto, o início de uma revolução, foi o que viu através de uma janela no corredor real. Sinceramente, já esperávamos por isso. A desigualdade entre os três estados é nítida, uma grande crise econômica autora de fomes e mortes, pensamentos iluministas, influências americanas, a lista continua, tudo contribui para o momento em que nos encontramos agora. “Não há como impedir que, essas pessoas não façam o que já está consumindo seus corpos” novamente pensou o bispo. Mas você pode escolher o que elas farão. .'
 var primeira_escolha2;
 
 var count = 0;
-function digitar() {
+function digitar() {//função que digita o texto na tela
 primeira_escolha = $("#primeira_escolha");
-window.setTimeout(function() { inserir(texto[count]) }, 30);
+window.setTimeout(function() { inserir(texto[count]) }, 30);//insere o texto
 primeiro_escolha2 =$("#primeira_escolha2");
 window.setTimeout(function() { inserir(textoviu[count]) }, 30);
 }
@@ -16,14 +16,16 @@ window.setTimeout(function() { inserir(textoviu[count]) }, 30);
 function inserir(letra) {
 primeira_escolha.innerHTML += letra;
 count++;
-if(count < texto.length)
+  
+if(count < texto.length){
 window.setTimeout(function() { inserir(texto[count]) }, 30);
-$("#primeira_escolha2").append(letra);
+$("#primeira_escolha").append(letra);
 count++;
-if(count < textoviu.length)
+}
+if(count < textoviu.length){
 window.setTimeout(function() { inserir(textoviu[count]) }, 30);
+$("#primeira_escolha2").append(letra);
 }
 
 window.onload = digitar;
-
 
