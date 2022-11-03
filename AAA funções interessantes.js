b563ee87295e79af0d5494f708740441b5c57926
maou4869 <115473213+maou4869@users.noreply.github.com>
2022-11-03 14:16:36 -0300

    Create AAA funções interessantes.js

---
+++ AAA funções interessantes.js
@@ -0,0 +1,8 @@
+function playerID() {//para o nome do jogador
+  $("#playerID").fadeIn(2000);//faz o nome do jogador aparecer
+}
+
+function troca() {//para trocar a "página" a ser exibida
+  $("#id_da_tag_a_ser_trocada").html('html da nova pagina')
+}
+
