ec1fcf99a677cbb151e55c34c7ad57f7c651d2f4
maou4869 <115473213+maou4869@users.noreply.github.com>
2022-11-03 14:19:35 -0300

    Update AAA funções interessantes.js

--- AAA funções interessantes.js
+++ AAA funções interessantes.js
@@ -1,8 +1,13 @@
+/*adicionar linha 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  para os códigos a seguir funcionarem*/
+
 function playerID() {//para o nome do jogador
   $("#playerID").fadeIn(2000);//faz o nome do jogador aparecer
 }
 
 function troca() {//para trocar a "página" a ser exibida
   $("#id_da_tag_a_ser_trocada").html('html da nova pagina')
+  //colocar text no lugar de html para trocar o texto somente, sem adicionar tags
 }
 
