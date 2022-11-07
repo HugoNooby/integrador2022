a9e9acc8badcaa6585329e177b8018d6b8b9a8e8
maou4869 <115473213+maou4869@users.noreply.github.com>
2022-11-07 10:50:03 -0300

    Update Bispo_Sieyès.js

--- Bispo_Sieyès.js
+++ Bispo_Sieyès.js
@@ -45,7 +45,7 @@ if(contador < texto.length){
 }
 
 }
-
+/*
 function digitacao4(texto, contador){
 
 if(contador < texto.length){
@@ -55,6 +55,15 @@ if(contador < texto.length){
 		digitacao4(texto, contador);
 	}, 30)
 
+}
+*/
+function typeWriter(txt,i) { //função
+	var speed = 50;
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
 }
 
 }
@@ -115,7 +124,7 @@ if(contador < texto.length){
 digitacao("É julho de 1789, todos estão reunidos no palácio, o rei sentado ao trono, o clero em suas rezas, a nobreza já em luto, e em frente, o terceiro estado. Uma sensação de reviravoltas invade o local. A proclamação da Assembleia Nacional Constituinte está acontecendo.", 0);
 digitacao2("Acredito que o Bispo Sieyès, para os íntimos Emmanuel Sieyès, seja opoente aos nobres. Ano passado publicou uma obra fascinante, depondo que a nobreza seria um fardo para nossa economia, graças aos seus gastos mirabolantes, 'Vou para fora tomar um pouco de ar', pensou o bispo levantando-se e saindo pela porta ao fundo. Observe que, enquanto um membro está a proclamar da assembleia, é divertido analisar as diferentes expressões que cada grupo realiza, alguns parecem decepcionados, outros satisfeitos e ainda outros surpresos, mas não tanto quanto Emmanuel Sieyès ao voltar de seu passeio, parece que descobriu algo inesperado... ou nem tanto assim.", 0);
 digitacao3("O povo estava unido, seus instrumentos de trabalho estavam empunhados em suas cansadas mãos, um protesto, o início de uma revolução, foi o que viu através de uma janela no corredor real. Sinceramente, já esperávamos por isso. A desigualdade entre os três estados é nítida, uma grande crise econômica autora de fomes e mortes, pensamentos iluministas, influências americanas, a lista continua, tudo contribui para o momento em que nos encontramos agora. “Não há como impedir que, essas pessoas não façam o que já está consumindo seus corpos” novamente pensou o bispo. Mas você pode escolher o que elas farão.", 0);
-digitacao4("Os guardas da entrada tentaram conter a multidão, no entanto, eram muitos os revoltosos. Invadiram o palácio e foram em direção ao salão de reuniões, no qual se encontravam os três estados. Todos se levantaram, entreolharam-se temerosos, a parte do terceiro estado que se achava a frente da assembleia se juntou ao povo, aumentando em grande proporção o volume de revolucionários. Bispo Sieyès observava tudo diante da fresta entre a parede e a porta, conseguia sentir a adrenalina deslizar em suas veias, o que fazer em um cenário como este?", 0);
+typewriter("Os guardas da entrada tentaram conter a multidão, no entanto, eram muitos os revoltosos. Invadiram o palácio e foram em direção ao salão de reuniões, no qual se encontravam os três estados. Todos se levantaram, entreolharam-se temerosos, a parte do terceiro estado que se achava a frente da assembleia se juntou ao povo, aumentando em grande proporção o volume de revolucionários. Bispo Sieyès observava tudo diante da fresta entre a parede e a porta, conseguia sentir a adrenalina deslizar em suas veias, o que fazer em um cenário como este?", 0);
 digitacao5("Fechou a porta e saiu em direção a cozinha, havia uma pequena porta de madeira que levava ao lado de fora do palácio, junto com os restos de alimento desperdiçados durante as refeições reais. Por um instante parou, olhou para os despojos e, se sentiu como eles, inspirou e seguiu seu caminho.", 0);
 digitacao6("É 1792, deixe-me contar o que aconteceu nos últimos três anos que se passaram. Emmanuel se tornou presidente da Assembleia Constituinte e integrante da Convenção Nacional, também um pouco arrogante, a fama como teórico sucumbiu sua mente, porém sua falta de habilidade como orador, reduziu sua competência política. Ano passado o rei Luís XVI tentou fugir de Paris, alguns estão dizendo que ele nos traiu. Amanhã é o dia da última sessão de reuniões da Convenção, estão votando a favor e contra a morte do rei, não sei o que pensar sobre isso, mas talvez eu saiba no que Bispo Sieyès votou.", 0);
 digitacao7("Seu voto mataria o rei, e o peso em sua consciência o afligiria pelo resto da vida. Mas nem todos pensavam assim. John Mcagainst, um dos membros, não suportava a ideia de ter como rei um traidor, e chegou à conclusão de que o voto contra a morte do soberano era uma traição ainda maior, então quis acabar com todos os que votaram a favor da vida de um rei infiel. No silêncio da madrugada enquanto o fazendeiro deixava garrafas de leite as portas das casas, dirigiu-se a elas e envenenou, com um elemento conhecido como arsênico, cada uma delas. Às cinco da tarde encontraram Emmanuel Sieyès no chão de sua cozinha, não havia mais ar em seus pulmões, apena seu olhar consciente e aturdido."<br><br>"“Não posso escolher como me sinto, mas posso escolher o que fazer a respeito.” — William Shakespeare.", 0);
