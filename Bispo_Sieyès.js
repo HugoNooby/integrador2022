0b4028ae44d7a335e3e5807ea6d64e4a72ecf7d2
maou4869 <115473213+maou4869@users.noreply.github.com>
2022-11-07 10:51:52 -0300

    Update Bispo_Sieyès.js

--- Bispo_Sieyès.js
+++ Bispo_Sieyès.js
@@ -60,7 +60,7 @@ if(contador < texto.length){
 function typeWriter(txt,i) { //função
 	var speed = 50;
   if (i < txt.length) {
-    document.getElementById("demo").innerHTML += txt.charAt(i);
+    document.getElementsByClassName(".osguardas").innerHTML += txt.charAt(i);
     i++;
     setTimeout(typeWriter, speed);
   }
