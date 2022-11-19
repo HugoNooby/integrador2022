d1d56225c1991999dc1c00ebb2ee45fd8532ba06
larabruna <115473490+Larinhaintegrador@users.noreply.github.com>
2022-11-19 13:32:22 -0300

    Update Bispo_Sieyès.js

--- Bispo_Sieyès.js
+++ Bispo_Sieyès.js
@@ -1,57 +1,13 @@
-const Ejulho = document.getElementById("Ejulho");
-const acreditoobserve = document.querySelector(".acreditoobserve");
-const Opovo = document.querySelector(".Opovo");
-const osguardas = document.querySelector(".osguardas");
-const fechouaporta = document.querySelector(".fechouaporta");
-const E1792 = document.querySelector(".E1792");
-const seuvoto = document.querySelector(".seuvoto");
-const guilhotina = document.querySelector(".guilhotina");
+var textnum = 0;
+var dialogue = ["É julho de 1789, todos estão reunidos no palácio, o rei sentado ao trono, o clero em suas rezas, a nobreza já em luto, e em frente, o terceiro estado. Uma sensação de reviravoltas invade o local. A proclamação da Assembleia Nacional Constituinte está acontecendo.", "Acredito que o Bispo Sieyès, para os íntimos Emmanuel Sieyès, seja opoente aos nobres. Ano passado publicou uma obra fascinante, depondo que a nobreza seria um fardo para nossa economia, graças aos seus gastos mirabolantes, 'Vou para fora tomar um pouco de ar', pensou o bispo levantando-se e saindo pela porta ao fundo.", "Observe que, enquanto um membro está a proclamar da assembleia, é divertido analisar as diferentes expressões que cada grupo realiza, alguns parecem decepcionados, outros satisfeitos e ainda outros surpresos, mas não tanto quanto Emmanuel Sieyès ao voltar de seu passeio, parece que descobriu algo inesperado... ou nem tanto assim.", "Escolha o rumo em que queira que a história tome (você poderá voltar caso mude de ideia):" ];
 
- document.querySelector(".titulo-principal");
-
-
-
-
-//função funcional
-function typeWrite(elemento){
-    const textoArray = elemento.innerHTML.split("");
-    elemento.innerHTML = '';
-    textoArray.forEach(function(letra,i){
-
-        setTimeout(function(){
-            elemento.innerHTML += letra;
-        },75*i)
-
-    });
-}
-
-function Mudarestado(el) {
-        var display = document.getElementById(el).style.display;
-        if(display == "block"){
-            document.getElementById(el).style.display = 'none';
-	}else{
-            document.getElementById(el).style.display = 'block';
-	}
-    }
-
-	
-typeWrite(Ejulho);
-typeWrite(acreditoobserve);
-typeWrite(Opovo);
-typeWrite(osguardas);
-typeWrite(fechouaporta);
-typeWrite(E1792);
-typeWrite(seuvoto);
-typeWrite(guilhotina);
-	
-	
-var num=0;
-function mostrar(){
-	if (num==0){
-		document.getElementById('minhaDiv').style.display='block';
-		num=1;
-	}else{
-		document.getElementById('minhaDiv').style.display='none';
-		num=0;
-	}
+function nextDialogue(){
+  document.getElementById("text").innerHTML = dialogue[textnum];
+  
+  if (textnum<2){
+    textnum++;
+  } else{
+    textnum = 0;
+  }
+  
 }
