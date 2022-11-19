314e3ac9a935eadeb0dc25b13e31b5b24858618e
maou4869 <115473213+maou4869@users.noreply.github.com>
2022-11-19 14:33:05 -0300

    Update Bispo_Sieyès.js

--- Bispo_Sieyès.js
+++ Bispo_Sieyès.js
@@ -1,11 +1,11 @@
-var textnum = -1;
+var textnum = 0;
 var dialogue = ["É julho de 1789, todos estão reunidos no palácio, o rei sentado ao trono, o clero em suas rezas, a nobreza já em luto, e em frente, o terceiro estado. Uma sensação de reviravoltas invade o local. A proclamação da Assembleia Nacional Constituinte está acontecendo.", "Acredito que o Bispo Sieyès, para os íntimos Emmanuel Sieyès, seja opoente aos nobres. Ano passado publicou uma obra fascinante, depondo que a nobreza seria um fardo para nossa economia, graças aos seus gastos mirabolantes, 'Vou para fora tomar um pouco de ar', pensou o bispo levantando-se e saindo pela porta ao fundo.", "Observe que, enquanto um membro está a proclamar da assembleia, é divertido analisar as diferentes expressões que cada grupo realiza, alguns parecem decepcionados, outros satisfeitos e ainda outros surpresos, mas não tanto quanto Emmanuel Sieyès ao voltar de seu passeio, parece que descobriu algo inesperado... ou nem tanto assim.", "Escolha o rumo em que queira que a história tome (você poderá voltar caso mude de ideia):" ];
 const text = document.getElementById("text");
 function nextDialogue(){
-  if (textnum<2){
+  document.getElementById("text").innerHTML = dialogue[textnum];
+  typeWrite(text);
+  if (textnum<=2){
     textnum++;
-    document.getElementById("text").innerHTML = dialogue[textnum];
-    typeWrite(text);
   }else{
     textnum = 0;
   }
