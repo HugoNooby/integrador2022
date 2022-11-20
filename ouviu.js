e5b7ad9be9fe2169754b6062d55cee0ecdd469cf
ylocinn <115472908+ylocinn@users.noreply.github.com>
2022-11-20 19:49:34 -0300

    Update ouviu.js

--- ouviu.js
+++ ouviu.js
@@ -31,3 +31,11 @@ function typeWrite(elemento){
 function mostra(){
   $("#seta").show();
 }
+
+$(window).ready(function () {
+
+  $(".main__nav-btn").click(function (){
+    $(".main_nav, .main_nav-btn").toggleClass("active");
+  });
+
+});
