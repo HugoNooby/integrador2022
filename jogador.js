ee1e5fe4462b89a9ac5751f6d6ba63701957f6d7
ylocinn <115472908+ylocinn@users.noreply.github.com>
2022-11-20 19:49:22 -0300

    Update jogador.js

--- jogador.js
+++ jogador.js
@@ -13,3 +13,10 @@ function myMove() {
     }
   }
 }
+$(window).ready(function () {
+
+  $(".main__nav-btn").click(function (){
+    $(".main_nav, .main_nav-btn").toggleClass("active");
+  });
+
+});
