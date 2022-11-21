151a03dd753565e060c6dac21b9f6643c91dc0a1
maou4869 <115473213+maou4869@users.noreply.github.com>
2022-11-21 00:43:59 -0300

    Update bispo_atacarprisao8.js

--- bispo_atacarprisao8.js
+++ bispo_atacarprisao8.js
@@ -3,13 +3,13 @@ var dialogue = ["14 de julho, Bastilha a prisão, o grande símbolo político e 
 const text = document.getElementById("text");
 function nextDialogue(){
   
-  if (textnum<=3){
+  if (textnum<=(dialogue.lenght-1)){
     $("#seta").hide();
     document.getElementById("text").innerHTML = dialogue[textnum];
     typeWrite(text);
     setTimeout(mostra(),5000);
     textnum++;
-  } else if (textnum==4){
+  } else if (textnum==dialogue.lenght){
     $('#box').html('<div class="data-container"><a href="bispoesconder.html" class="button">SE ESCONDER</a></div><br><div class="data-container"><a href="bispojuntar.html" class="button">SE JUNTAR AO POVO</a></div><br><div class="data-container"><a href="viu.html" class="button">VOLTAR E TENTAR NOVAMENTE</a></div>') ;
   }else{
     textnum = 0;
