c81da01860bf8525cae76c326b32833c4ed657c0
ylocinn <115472908+ylocinn@users.noreply.github.com>
2022-11-22 10:16:20 -0300

    Update slide.js

--- slide.js
+++ slide.js
@@ -1,19 +1,3 @@
-let slideIndex = [1,1];
-let slideId = ["mySlides1", "mySlides2"]
-showSlides(1, 0);
-showSlides(1, 1);
-
-function plusSlides(n, no) {
-  showSlides(slideIndex[no] += n, no);
-}
-
-function showSlides(n, no) {
-  let i;
-  let x = document.getElementsByClassName(slideId[no]);
-  if (n > x.length) {slideIndex[no] = 1}    
-  if (n < 1) {slideIndex[no] = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";  
-  }
-  x[slideIndex[no]-1].style.display = "block";  
+function mudaTexto(){
+	document.getElementById('muda').innerHTML="Atividade prática da disciplina de Desenvolvimento Web, ministrada pelo professor Josef Augusto.";
 }
