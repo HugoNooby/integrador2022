b5386c97bc8ee658d65b1a58632879235bd92ffd
ylocinn <115472908+ylocinn@users.noreply.github.com>
2022-11-23 23:41:23 -0300

    Update n.js

--- n.js
+++ n.js
@@ -6,15 +6,18 @@ function pesquisa() {
   } else if (personagem == "Scott") {
     document.getElementById("resultado").innerHTML =
       "Uau! Se esse jovem bonito (e lobisomem) fosse o narrador, bem... não passaria despercebido. Dependendo da temporada, Scott Mccall é Técnico veterinário, Co-Capitão da Equipe Lacrosse, Assistente de Treinador de Lacrosse, Estudante universitário ou um Veterinário. Não, ele não é o narrador, tente outra vez!";
-  } else if ((personagem == "Josef") | (personagem == "Samwise")) {
+  } else if (personagem == "Josef") {
     document.getElementById("resultado").innerHTML =
       "Se eu estivesse no seu lugar, com toda a certeza teria desconfiado de Josef, você pensa como eu. Josef Auguto é graduado em Engenharia da Computação pela Pontifícia Universidade Católica de Goiás, atualmente é professor do Departamento de Informática do Instituto Federal Goiano - Campus Ceres (Lattes: http://lattes.cnpq.br/2393555810493937). Não é ele, tente novamente!";
-  } else if ((personagem == "Maria") | (personagem == "Meriadoc")) {
+  } else if (personagem == "Maria") {
     document.getElementById("resultado").innerHTML =
       "Achou mesmo que Maria Antonieta teria tempo para observar alguém tão peculiar como Sieyès? Não é ela o narrador, Maria é a arquiduquesa da Áustria e esposa do rei Luís XVI. Tente outra vez!";
-  } else if ((personagem == "") | (personagem == "Peregrin")) {
+  } else if (personagem == "") {
     document.getElementById("resultado").innerHTML =
       "";
+  } else if (personagem == "Jughead") {
+    document.getElementById("resultado").innerHTML =
+      "Hummmmmm, esse foi um ótimo chute! Jughead Jones é Escritor, consequentemente narrador da história de sua cidade, mas não é o narrador desta história. Jug também é Estudante do Ensino Médio, Operador de Estande de Projeção, Membro/Líder de Gangue (não siga este exemplo). Tente de novo!";
   } else if (personagem == "") {
     document.getElementById("resultado").innerHTML =
       "";
@@ -24,10 +27,7 @@ function pesquisa() {
   } else if (personagem == "") {
     document.getElementById("resultado").innerHTML =
       "";
-  } else if ((personagem == "Gandalf") | (personagem == "Mithrandir")) {
-    document.getElementById("resultado").innerHTML =
-      "";
-  } else if ((personagem == "") | (personagem == "Harry Potter")) {
+  } else if (personagem == ""){
     document.getElementById("resultado").innerHTML = 
     "";
   } else {
